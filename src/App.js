57198786c97ca64fb07a18a612bee1bfb314b19d
unknown <gowthambujju02@gmail.com>
2022-06-20 13:45:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Search from './components/Search';
+
+function App() {
+  return (
+    <div><Search /></div>
+  );
+}
+
+export default App;
